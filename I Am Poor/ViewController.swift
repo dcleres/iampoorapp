39ed3d940e834ed2cfbd3a571f7de2db2fd35549
David Cleres <davidcleres@gmail.com>
2020-01-02 22:04:58 +0100

    my first ios app

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by David Cleres on 02.01.20.
+//  Copyright © 2020 David Cleres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
